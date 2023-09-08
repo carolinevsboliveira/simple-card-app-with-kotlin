ddb529ef33be7c3a75926655138d550f1d422656
carolinevsboliveira <carolinevsboliveira@gmail.com>
2023-09-07 22:42:47 -0300

    feat: add image component

--- app/src/main/java/com/example/udemy_project_twitter_card_project_1/MainActivity.kt
+++ app/src/main/java/com/example/udemy_project_twitter_card_project_1/MainActivity.kt
@@ -3,15 +3,30 @@ package com.example.udemy_project_twitter_card_project_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.udemy_project_twitter_card_project_1.ui.theme.TwitterCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +35,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             TwitterCardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
                     CreateBizCard()
                 }
             }
@@ -29,19 +46,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CreateBizCard(){
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
+fun CreateBizCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(23.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = CardDefaults.cardElevation(defaultElevation = 12.dp)
+        ) {
+            PrincessImage()
+        }
+    }
+}
 
+@Composable
+fun PrincessImage() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, color = Color.LightGray),
+        shadowElevation = 4.dp
+    ) {
+        Image(painter = painterResource(id = R.drawable.princess), contentDescription = "princess", contentScale = ContentScale.FillWidth)
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
