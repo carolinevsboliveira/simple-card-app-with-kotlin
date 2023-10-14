ae3831ab3a6e03d15fb3bd7541730b4bb5846cc0
carolinevsboliveira <carolinevsboliveira@gmail.com>
2023-10-13 23:14:54 -0300

    feat: add list from array

--- app/src/main/java/com/example/udemy_project_twitter_card_project_1/MainActivity.kt
+++ app/src/main/java/com/example/udemy_project_twitter_card_project_1/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.udemy_project_twitter_card_project_1
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,10 +17,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.material3.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
 
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.udemy_project_twitter_card_project_1.ui.theme.TwitterCardTheme
 
@@ -41,6 +47,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val portifolioButtonHandler = remember {
+        mutableStateOf(false)
+
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -64,10 +74,7 @@ fun CreateBizCard() {
                     modifier = Modifier.size(150.dp)
                         .padding(5.dp)
                 )
-                Divider(
-                    thickness = 1.dp,
-                    modifier = Modifier.padding(10.dp)
-                )
+                SimpleDivider()
                 Column(
                     modifier = Modifier.padding(10.dp),
                     verticalArrangement = Arrangement.spacedBy(10.dp),
@@ -78,17 +85,30 @@ fun CreateBizCard() {
                         "Mrs. Queen of America",
                         "Some loved queen"
                     )
-                    FilledButton("Say Hi!")
+                    FilledButton(
+                        "Say Hi!",
+                        onClick = { portifolioButtonHandler.value = !portifolioButtonHandler.value })
+                    if (portifolioButtonHandler.value) {
+                        Content()
+                    } else Box() {}
+
                 }
+
             }
 
         }
+
     }
 }
 
 @Composable
+fun SimpleDivider(thickness: Dp = 3.dp) {
+    Divider(thickness = thickness, color = Color.LightGray)
+}
+
+@Composable
 fun PrincessImage(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Surface(
         modifier,
@@ -119,8 +139,58 @@ fun InformationText(text: String, description: String) {
 }
 
 @Composable
-fun FilledButton(title: String) {
-    Button(onClick = { Log.d("Main Button", "Has Clicked") }) {
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Portifolio(listOf("Alicia", "Marina", "Martinez"))
+
+
+        }
+    }
+}
+
+@Composable
+fun PortifolioRow(header: String) {
+    Card(
+        modifier = Modifier.padding(8.dp)
+            .background(color = Color.Transparent)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(15.dp)), elevation = CardDefaults.elevatedCardElevation(4.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            PrincessImage(
+                modifier = Modifier.size(100.dp)
+                    .padding(5.dp)
+            )
+            Text(text = header, fontWeight = FontWeight.Bold)
+        }
+
+
+    }
+}
+
+@Composable
+fun Portifolio(data: List<String>) {
+    LazyColumn {
+        items(data) { text ->
+            PortifolioRow(header = text)
+        }
+    }
+
+}
+
+@Composable
+fun FilledButton(title: String, onClick: () -> Unit) {
+    Button(onClick = onClick) {
         Text(title)
     }
 
@@ -131,5 +201,6 @@ fun FilledButton(title: String) {
 fun GreetingPreview() {
     TwitterCardTheme {
         CreateBizCard()
+
     }
 }
